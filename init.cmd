cca3acbb831122e879b49f0c3c956b472436da71
Ruslan Keba <rukeba@gmail.com>
2020-04-28 00:06:25 +0300

    init.cmd updated with frontend build

--- init.cmd
+++ init.cmd
@@ -1,6 +1,15 @@
+pushd %~dp0
+
 if not exist venv: virtualenv venv
 
 call venv\scripts\activate
 pip install -r requirements.txt
 python -m spacy download en_core_web_lg
 python manage.py migrate
+
+pushd fronend
+yarn install
+yarn build
+popd
+
+popd
